79699af74c1797228d33ea001495b0196d8b466c
Leonardo Araújo <leonardo.araujornb@gmail.com>
2024-01-21 12:27:30 -0300

    primeiro commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# copybase-uploadplanner
--- api/src/upload.js
+++ api/src/upload.js
@@ -1,1 +1,20 @@
-<?php
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+let upload = multer({
+    destination: './assets/uploads/'
+})
+
+router.post('/', upload.array('file'), async (request, response) => {
+    response.status(200).send({
+        message: 'Deu certo'
+    })
+    /*
+     response.send({
+         upload: true,
+         files: request.files
+     });*/
+});
+
+module.exports = router;
